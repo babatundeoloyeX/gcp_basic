83c49a3ba05741ffa4490b580678096c8d919c68
Mookie WAF <balericaclass6@gmail.com>
2024-12-30 16:31:54 -0500

    Add files via upload

---
+++ 9-serviceaccount.tf
@@ -0,0 +1,18 @@
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_service_account
+resource "google_service_account" "service-a" {
+  account_id = "thailand"
+}
+
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project_iam
+resource "google_project_iam_member" "service-a" {
+  project = "thailand"
+  role    = "roles/storage.admin"
+  member  = "serviceAccount:${google_service_account.service-a.email}"
+}
+
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_service_account_iam
+resource "google_service_account_iam_member" "service-a" {
+  service_account_id = google_service_account.service-a.id
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "serviceAccount:thailand.svc.id.goog[staging/service-a]"
+}
